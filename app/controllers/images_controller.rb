2295ae7682220664fd18d63dd0c4d6733e788990
leroi dejesa & peter <student@epicodus.com>
2015-07-08 11:26:50 -0700

    add upload image functionality

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,14 +1,24 @@
 class ImagesController < ApplicationController
   def index
+    @images = Image.all
   end
 
   def show
+
   end
 
   def new
+    @image = current_user.images.build
   end
 
   def create
+    @image = current_user.images.build(image_params)
+    if @image.save
+      flash[:notice] = "Upload successful!"
+      redirect_to images_path
+    else
+      render :new
+    end
   end
 
   def edit
@@ -18,5 +28,10 @@ class ImagesController < ApplicationController
   end
 
   def destroy
+  end
+
+private
+  def image_params
+    params.require(:image).permit(:description)
   end
 end
