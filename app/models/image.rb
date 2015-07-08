f1005d5e26b28068b59390ddc79e61fc102e816c
leroi dejesa & peter <student@epicodus.com>
2015-07-08 15:05:10 -0700

    add additional image sizes in image model

--- app/models/image.rb
+++ app/models/image.rb
@@ -3,6 +3,10 @@ class Image < ActiveRecord::Base
   belongs_to :user
   validates :description, :presence => true
 
-  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
+
+
+  has_attached_file :image, styles: { medium: "500x500>", thumb: "100x100>", large: "800x800>", xlarge: "1200x1200>" }
+  #set image sizes before uploading images so it can reference url  i.e url(:large)
+
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
